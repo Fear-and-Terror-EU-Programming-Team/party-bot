ac5dd209d767ad00e9aa606fdfcf078112043cdb
LolloAntoni28 <lorant04@outlook.it>
2019-10-16 18:34:15 +0200

    Updated code

--- Tarkov_bot.py
+++ Tarkov_bot.py
@@ -5,14 +5,24 @@
 
 # IMPORT
 import discord
+from discord.utils import get
+import json
 
 # SERVER VARIABLES (SERVER SPECIFIC)
-allowed_channels = ["general"]  # list of allowed channels where the bot reads commands from
-allowed_role = "Moderator" # users with this role will be able to close by forcefully any raid
 bot_commands = ["$StartRaid", "$CloseRaid", "$Help", "$ForceClose"]  # list with all the bot commands (don't change the order!)
+
+allowed_channels = ["general"]  # list of allowed channels where the bot reads commands from
+allowed_role = "Moderator"  # users with this role will be able to close by forcefully any raid
+
 raid_notification = "631524437879160874"  # role (id, str) the bot tags when announcing raid
-raid_emoji = "✅"     # emoji to be reacted to join the raid
+raid_notification_emoji = "✅"     # emoji (emoji only, no id allowed) to be reacted to in order to receive the raid notification role
+raid_notification_channel_id = 633736020738703400   # channel (id, int) in which the bot assigns the raid notification role
+raid_notification_role = "Tarkov"  # role to be assigned when reacting to the raid notification role assignment message
+
+raid_emoji = "✅"     # emoji (emoji only, no id allowed) to be reacted to join the raid
 raid_channel_id = 631525965704724517   # channel (id, int) in which the bot announces a raid
+
+database_directory = "database.json"  # directory of the .json file to be used for some short data storage
 
 # SYSTEM VARIABLES (ONLY TOKEN TO BE CHANGED)
 token = "NjMxNDkzMTE4NDk1MjkzNDQw.XaOG9Q.WH6HmfY7ftNkpEfWMpx_gVERseo"
@@ -25,7 +35,72 @@ raid_message = None
 raid_members = []
 raid_active = False
 
+# FUNCTIONS
+
+
+def read_database(dictionary, key=None):
+	####################################################################################################################
+	# Author: ʝυʂƚ α ɳσɾɱαʅ ɠυყ
+	# DateCreated: 10 / 16 / 2019
+	# Purpose: To read a key from a dictionary in the .json file
+	####################################################################################################################
+
+	# VARIABLES
+	global database_directory
+
+	# READ
+	current_file = open(database_directory, "r")
+	json_text = json.load(current_file)
+	current_file.close()
+
+	# RETURN
+	if dictionary == "text":
+		result = json_text
+	else:
+		result = json_text[dictionary][key]
+	return result
+
+
+def write_database(dictionary, key, new_value):
+	####################################################################################################################
+	# Author: ʝυʂƚ α ɳσɾɱαʅ ɠυყ
+	# DateCreated: 10 / 16 / 2019
+	# Purpose: To replace the value of a key from a dictionary in the .json file
+	####################################################################################################################
+
+	# VARIABLES
+	global database_directory
+
+	# READ
+	current = read_database(dictionary, key)
+	json_text = read_database("text")
+
+	# WRITE
+	json_text[dictionary][key] = json_text[dictionary][key].replace(current, str(new_value))
+	current_file = open(database_directory, "w")
+	json.dump(json_text, current_file, indent=2)
+	current_file.close()
+
 # CODE
+@client.event
+async def on_ready():
+	####################################################################################################################
+	# Author: ʝυʂƚ α ɳσɾɱαʅ ɠυყ
+	# DateCreated: 10 / 15 / 2019
+	# Purpose: To execute a line of code when the bot becomes online
+	####################################################################################################################
+	# VARIABLES
+	global raid_notification_channel_id
+	raid_notification_channel = client.get_channel(raid_notification_channel_id)  # channel (id) in which the bot announces a raid
+	notification_message_id = read_database("tarkov_bot", "notification_message_id")
+
+	# CODE
+	if notification_message_id != "None":
+		await client.http.delete_message(raid_notification_channel.id, notification_message_id)
+	await raid_notification_channel.send(f"If you want to be notified whenever a raid starts please react down "
+										f"below with {raid_notification_emoji}.")
+
+
 @client.event
 async def on_message(message):
 	####################################################################################################################
@@ -40,6 +115,7 @@ async def on_message(message):
 	global raid_active
 	global raid_members
 	raid_channel = client.get_channel(raid_channel_id)  # channel (id) in which the bot announces a raid
+	server_members = client.get_all_members()   # all server members
 	raid_text = f"> <@&{raid_notification}> <@{message.author.id}> has started a raid!\n" \
 					f"> React to this message with {raid_emoji} if you want to join it!\n" \
 					f"> Remember that the max amount of people to join is 5. So react fast!"
@@ -55,6 +131,7 @@ async def on_message(message):
 			if message.content in bot_commands:
 
 				# COMMANDS
+				# $StartRaid
 				if message.content.find(bot_commands[0]) != -1:
 					if raid_active is False:
 						raid_active = True
@@ -63,21 +140,26 @@ async def on_message(message):
 					else:
 						await message.channel.send("A raid is already active.")
 
+				# $CloseRaid
 				elif message.content.find(bot_commands[1]) != -1:
 					if message.author == raid_leader:
 						await message.channel.send(f"<@{message.author.id}> your raid has been successfully closed.")
-						await raid_message.edit(content="This raid has been closed by its leader.")
+						await raid_message.edit(content="> This raid has been closed by its leader.")
 						raid_leader = None
 						raid_active = False
 						for user in raid_members:
-							await user.send("The raid you joined has been closed by its leader.")
+							await user.send("> The raid you joined has been closed by its leader.")
+						for user in server_members:
+							await raid_message.remove_reaction(raid_emoji, user)
 						raid_members.clear()
 					else:
 						await message.channel.send("You don't have an active raid.")
 
+				# $Help
 				elif message.content.find(bot_commands[2]) != -1:
 					await message.channel.send(help_text)
 
+				# $ForceClose
 				elif message.content.find(bot_commands[3]) != -1:
 					if allowed_role in str(message.author.roles):
 						if raid_leader is None:
@@ -85,9 +167,11 @@ async def on_message(message):
 						else:
 							await message.channel.send(f"<@{raid_leader.id}> your raid was forcibly closed by "
 														f"<@{message.author.id}>.")
-							await raid_message.edit(content=f"This raid was forcibly closed by <@{message.author.id}>.")
+							await raid_message.edit(content=f"> This raid was forcibly closed by <@{message.author.id}>.")
 							for user in raid_members:
-								await raid_message.remove_reaction(raid_emoji)
+								await user.send(f"> The raid you joined was forcibly closed by <@{message.author.id}>.")
+							for user in server_members:
+								await raid_message.remove_reaction(raid_emoji, user)
 							raid_leader = None
 							raid_active = False
 							raid_members.clear()
@@ -97,6 +181,9 @@ async def on_message(message):
 		if message.channel.id == raid_channel_id:
 			raid_message = message
 			await message.add_reaction(raid_emoji)
+		elif message.channel.id == raid_notification_channel_id:
+			await message.add_reaction(raid_notification_emoji)
+			write_database("tarkov_bot", "notification_message_id", message.id)
 
 
 @client.event
@@ -109,6 +196,7 @@ async def on_reaction_add(reaction, user):
 
 	# VARIABLES
 	global raid_members
+	# discord.utils.get(server.roles, name="Tarkov")
 	try:
 		raid_text = f"> <@&{raid_notification}> <@{raid_leader.id}> has started a raid!\n" \
 						f"> React to this message with {raid_emoji} if you want to join it!\n" \
@@ -121,9 +209,14 @@ async def on_reaction_add(reaction, user):
 		if str(reaction.message.author) == bot_client:
 			if (str(user) != bot_client) and (str(user) not in raid_members):
 				if "Remember that the max amount of people to join is 5. So react fast!" in reaction.message.content:
-					raid_members.append(user)
-					await raid_leader.send(f"{user} joined your raid!")
-					await reaction.message.edit(content=(raid_text + f"\n> - <@{user.id}>"))
+					if user not in raid_members:
+						raid_members.append(user)
+						await raid_leader.send(f"{user} joined your raid!")
+						await reaction.message.edit(content=(reaction.message.content + f"\n> - <@{user.id}>"))
+	elif reaction.message.channel.id == raid_notification_channel_id:
+		if str(reaction.message.author) == bot_client:
+			if (str(user) != bot_client) and (raid_notification_role not in str(user.roles)):
+				await user.add_roles(user, raid_notification_role)
 
 
 client.run(token)
